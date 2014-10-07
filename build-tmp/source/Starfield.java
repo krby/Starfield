1e9414843978bfa4a8a43a564cb1de6fc8d11ac0
kichoy <kichoy@697-334-Sauron.sf.k12.internal>
2014-10-07 11:36:40 -0700

    started it, working

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,8 +1,24 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 NormalParticle[] particles;
 int numParticles  = 100;
 
 int scrnSz = 512;
-void setup()
+public void setup()
 {
 	size(scrnSz, scrnSz);
 
@@ -13,7 +29,7 @@ void setup()
 	}
 }
 
-void draw()
+public void draw()
 {
 	background(0, 10);
 	for(int i = 0; i < particles.length; i++)
@@ -23,7 +39,7 @@ void draw()
 	}
 }
 
-void mousePressed()
+public void mousePressed()
 {
 	for(int i = 0; i < particles.length; i++)
 	{
@@ -49,13 +65,13 @@ class NormalParticle
 		partSize = (int)(Math.random()*3+1);
 	}
 
-	void move()
+	public void move()
 	{
 		dx = dx + Math.cos(dTheta)*dSpeed;
 		dy = dy + Math.sin(dTheta)*dSpeed;
 	}
 
-	void show()
+	public void show()
 	{
 		fill(myColor); 
 		noStroke();
@@ -71,4 +87,13 @@ interface Particle
 class OddballParticle
 {
 	//your code here
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
